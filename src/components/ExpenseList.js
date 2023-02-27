f7a162b3b29b3f07fcd80d9a6cc6f5e19f7b6288
Jesus Meza <jsmsmail@gmail.com>
2023-02-27 17:30:32 -0600

    Budget Allocation, ExpenseList, Expense Item and Allocation Form

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,34 @@
+import React from "react";
+import ExpenseItem from './ExpenseItem';
+import { AppContext } from "../context/AppContext";
 
+
+const ExpenseList = () => {
+    const { expenses } = React.useContext(AppContext);
+
+    return (
+        <table className='table'>
+            <thead className='thread-light'>
+                <tr>
+                    <th scope='col1'>Department</th>
+                    <th scope='col1'>Allocated Budget</th>
+                    <th scope='col1'>Increase</th>
+                    <th scope='col1'>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map( expense => (
+                    <ExpenseItem
+                        id={expense.id}
+                        key={expense.id}
+                        name={expense.name}
+                        cost={expense.cost}
+                    />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+
+export default ExpenseList;
